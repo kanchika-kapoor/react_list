9f0d225ba9fbaa2c5758db10d7deda60ab8f7472
kanchika-kapoor <kanchika.ai@gmail.com>
2024-03-19 04:39:06 +0530

    adding state to guestlist

--- src/state/GuestList.tsx
+++ src/state/GuestList.tsx
@@ -1,3 +1,12 @@
+import { useState } from "react"
+
 export const GuestList:React.FC= ()=>{
-    return <div></div>
+    const [name, setName] = useState('')
+
+
+    return <div>
+        <h2>GuestList</h2>
+        <input type="text" value={name} onChange = {(e)=>{setName(e.target.value)}}/>
+        <button>Add Guest</button>
+    </div>
 }
