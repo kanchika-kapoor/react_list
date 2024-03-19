f6e8f66a8afe4e5fe1109516e35190114fa889ff
kanchika-kapoor <kanchika.ai@gmail.com>
2024-03-19 15:13:34 +0530

    action creator added

---
+++ redux-ts/src/state/action-creators/index.ts
@@ -0,0 +1,33 @@
+import axios from "axios"
+import { ActionTypes } from "../action-types"
+import { Action } from "../actions"
+import { Dispatch } from "redux"
+
+export const SearchRepositories = (term: string) =>{
+    return async (dispatch: Dispatch<Action>)=>{
+        dispatch({
+            type: ActionTypes.SEARCH_REPOSITORIES
+        })
+
+        try{
+            const {data} = await axios.get('https://registry.npmjs.org/-/v1/search', {params:{text: term}})
+
+            const names: string[] = data.map((res)=>{
+                return res.package.name
+            })
+
+            dispatch({
+                type: ActionTypes.SEARCH_REPOSITORIES_SUCCESS,
+                payload: names
+            })
+
+
+        } catch(err)
+        {
+            dispatch({
+                type: ActionTypes.SEARCH_REPOSITORIES_ERROR,
+                payload: (<Error>err).message
+            })
+        }
+    }
+}
