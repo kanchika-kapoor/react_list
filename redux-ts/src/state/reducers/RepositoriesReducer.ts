632173433034315c0d82ed35072e932333c5f5c0
kanchika-kapoor <kanchika.ai@gmail.com>
2024-03-19 14:38:58 +0530

    reformating reducer

--- redux-ts/src/state/reducers/RepositoriesReducer.ts
+++ redux-ts/src/state/reducers/RepositoriesReducer.ts
@@ -4,26 +4,47 @@ interface RepositioriesState {
     data: string[]
 }
 
-interface Action {
-    type: string
-    payload?: any
+interface SearchRepositoriesAction {
+    type: ActionTypes.SEARCH_REPOSITORIES
+}
+
+interface SearchRepositoriesSuccess {
+    type: ActionTypes.SEARCH_REPOSITORIES_SUCCESS,
+    payload: string[]
+}
+
+interface SearchRepositoriesError {
+    type: ActionTypes.SEARCH_REPOSITORIES_ERROR,
+    payload: string
+}
+
+// type union
+type Action = SearchRepositoriesAction|
+SearchRepositoriesSuccess|
+SearchRepositoriesError
+
+enum ActionTypes{
+    'SEARCH_REPOSITORIES' = 'search_repositories',
+    'SEARCH_REPOSITORIES_SUCCESS' = 'search_repositories_success',
+    'SEARCH_REPOSITORIES_ERROR'= 'search_repositories_error'
 }
 
 const reducer = (state: RepositioriesState, action: Action):RepositioriesState=>{
+    // type guard
     switch (action.type) {
-        case 'search_repositories':
+        case ActionTypes.SEARCH_REPOSITORIES:
             return {
                 loading: true,
                 error: null,
                 data: []
             }
-        case 'search_repositories_success':
+        case ActionTypes.SEARCH_REPOSITORIES_SUCCESS:
             return {
                 loading: false,
                 error: null,
                 data: action.payload
             }
-        case 'search_repositories_error':
+        case ActionTypes.SEARCH_REPOSITORIES_ERROR:
             return {
                 loading: false,
                 error: action.payload,
