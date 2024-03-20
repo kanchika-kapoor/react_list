de9ff803a1c722680e178e655d08e99b32af4563
kanchika-kapoor <kanchika.ai@gmail.com>
2024-03-20 05:42:23 +0530

    integrate dispatch to form

--- redux-ts/src/components/RepositoriesList.tsx
+++ redux-ts/src/components/RepositoriesList.tsx
@@ -1,12 +1,22 @@
+import {useState} from 'react'
+import { useDispatch } from 'react-redux'
+import { actionCreators } from '../state'
+
 const RepositoriesList:React.FC = ()=>{
+    const [term, setTerm] = useState('')
+    const dispatch = useDispatch()
+
     const onSubmit = (e: React.FormEvent<HTMLFormElement>)=>{
         e.preventDefault()
+        dispatch<any>(actionCreators.SearchRepositories(term))
     }
 
     return (
         <div>
             <form onSubmit={onSubmit}>
-                <input type="text" />
+                <input type="text" value={term} onChange={
+                    (e)=>setTerm(e.target.value)
+                }/>
                 <button>Search</button>
             </form>
         </div>
